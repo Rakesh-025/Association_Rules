4b0c160ce906087bb991c8957e33cfb0b363a0f9
Rakesh-025 <107669272+Rakesh-025@users.noreply.github.com>
2022-07-26 07:25:40 +0530

    Add files via upload

---
+++ Association Rules for Books.py
@@ -0,0 +1,62 @@
+# Implementing Apriori algorithm from mlxtend
+
+# conda install mlxtend
+# or
+# pip install mlxtend
+
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+import matplotlib.pyplot as plt
+
+X= pd.read_csv(r"C:\Users\kaval\OneDrive\Desktop\360digit\datatypes\book.csv")
+X.shape
+X.columns
+import matplotlib.pyplot as plt
+
+frequent_itemsets = apriori(X, min_support = 0.0075, max_len = 4, use_colnames = True)
+
+# Most Frequent item sets based on support 
+frequent_itemsets.sort_values('support', ascending = False, inplace = True)
+
+k = ('red','blue','green','violet','yellow')
+plt.bar(x = list(range(0, 11)), height = frequent_itemsets.support[0:11], color =k)
+plt.xticks(list(range(0, 11)), frequent_itemsets.itemsets[0:11], rotation=20)
+plt.xlabel('item-sets')
+plt.ylabel('support')
+plt.show()
+# From the Visualisation  we had seen that cookBks has the highest support is 0.431
+
+rules = association_rules(frequent_itemsets, metric = "lift", min_threshold = 1)
+rules.head(20)
+rules.sort_values('lift', ascending = False).head(10)
+
+################################# Extra part ###################################
+def to_list(i):
+    return (sorted(list(i)))
+
+ma_X = rules.antecedents.apply(to_list) + rules.consequents.apply(to_list)
+
+ma_X = ma_X.apply(sorted)
+
+rules_sets = list(ma_X)
+
+unique_rules_sets = [list(m) for m in set(tuple(i) for i in rules_sets)]
+
+index_rules = []
+
+for i in unique_rules_sets:
+    index_rules.append(rules_sets.index(i))
+
+# getting rules without any redudancy 
+rules_no_redudancy = rules.iloc[index_rules, :]
+
+# Sorting them with respect to list and getting top 10 rules 
+R=rules_no_redudancy.sort_values('lift', ascending = False).head(10)
+
+# 1718	({'GeogBks', 'ItalAtlas'})   # 	antecedents
+# 1718	({'RefBks'})	consequents 
+# These  books have the highest Lift ratio i.e.,   4.662004662004662
+
+
+
+
---
+++ Myphone data.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+import matplotlib.pyplot as plt
+df= pd.read_csv(r"C:\Users\kaval\OneDrive\Desktop\360digit\datatypes\myphonedata.csv")
+
+
+# drop emp_name column
+df.drop(['V1','V2','V3'], axis=1, inplace=True)
+df.dtypes
+
+
+frequent_itemsets = apriori(df, min_support = 0.0075, max_len = 4, use_colnames = True)
+
+# Most Frequent item sets based on support 
+frequent_itemsets.sort_values('support', ascending = False, inplace = True)
+
+plt.bar(x = list(range(0, 11)), height = frequent_itemsets.support[0:11], color ='red')
+plt.xticks(list(range(0, 11)), frequent_itemsets.itemsets[0:11], rotation=20)
+plt.xlabel('item-sets')
+plt.ylabel('support')
+plt.show()
+
+rules = association_rules(frequent_itemsets, metric = "lift", min_threshold = 1)
+rules.head(20)
+rules.sort_values('lift', ascending = False).head(10)
+
+################################# Extra part ###################################
+def to_list(i):
+    return (sorted(list(i)))
+
+ma_X = rules.antecedents.apply(to_list) + rules.consequents.apply(to_list)
+
+ma_X = ma_X.apply(sorted)
+
+rules_sets = list(ma_X)
+
+unique_rules_sets = [list(m) for m in set(tuple(i) for i in rules_sets)]
+
+index_rules = []
+
+for i in unique_rules_sets:
+    index_rules.append(rules_sets.index(i))
+
+# getting rules without any redudancy 
+rules_no_redudancy = rules.iloc[index_rules, :]
+
+# Sorting them with respect to list and getting top 10 rules 
+l=rules_no_redudancy.sort_values('lift', ascending = False).head(10)
+    
